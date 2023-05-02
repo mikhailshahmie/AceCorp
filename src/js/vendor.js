5f9e721ed9d91a775314dc366c5c01ddc402c317
XNORF <78629634+XNORF@users.noreply.github.com>
2023-05-02 17:26:05 +0800

    initialize

---
+++ src/js/vendor.js
@@ -0,0 +1,11 @@
+import "jquery";
+
+import AOS from "aos";
+import Isotope from "isotope-layout";
+import Waypoint from "waypoints/lib/noframework.waypoints.min.js";
+import GLightbox from "glightbox";
+import Swiper from "swiper/bundle";
+
+import "boxicons/css/boxicons.min.css";
+import "remixicon/fonts/remixicon.css";
+import "swiper/css/bundle";
