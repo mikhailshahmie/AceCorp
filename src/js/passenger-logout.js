4b4d12cb9e76a90ab8838065316b5937dc2caa22
mikhailshahmie <mikhail.shahmie@gmail.com>
2023-05-04 03:34:44 +0800

    Create passenger-logout.js
    -base of passenger logout

---
+++ src/js/passenger-logout.js
@@ -0,0 +1,8 @@
+const main = require("./main.js"); //MUST HAVE
+/*function logout() {
+    if (confirm("Are you sure you want to log out?")) {
+      // Perform logout action
+      window.location.href = "index.html";
+    }
+  }*/
+  
