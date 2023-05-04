22ac2301c8341c51551f9edebaaf4f083ac0bb3f
Amirul Irfan <amirulirfan.utm@gmail.com>
2023-05-04 16:35:09 +0800

    minor change

--- src/js/signin.js
+++ src/js/signin.js
@@ -2,28 +2,32 @@
 const main = require("./main.js");
 let userData;
 main.onAuthStateChanged(main.auth, (user) => {
-    if (user) {
-        main.getDoc(main.doc(main.db, "users", user.uid)).then((doc) => {
-            userData = doc.data();
-            window.location.href = "/index.html";
-        });
-    } else {
-    }
+  if (user) {
+    main.getDoc(main.doc(main.db, "users", user.uid)).then((doc) => {
+      userData = doc.data();
+
+      if (user.emailVerified) {
+        window.location.href = "/index.html";
+      }
+    });
+  } else {
+  }
 });
 /////////////////////////////////////////////////////////////////
 
 const loginForm = document.querySelector("#loginForm");
 
 loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const email = loginForm.email.value;
-    const password = loginForm.password.value;
-    main.signInWithEmailAndPassword(main.auth, email, password)
-        .then((cred) => {
-            alert("Sign in successful");
-            window.location.href = "/index.html";
-        })
-        .catch((error) => {
-            alert(error.message);
-        });
+  e.preventDefault();
+  const email = loginForm.email.value;
+  const password = loginForm.password.value;
+  main
+    .signInWithEmailAndPassword(main.auth, email, password)
+    .then((cred) => {
+      alert("Sign in successful");
+      window.location.href = "/index.html";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
 });
