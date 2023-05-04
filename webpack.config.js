ea21230116e53a89478752af6f9c5f92960aa370
XNORF <78629634+XNORF@users.noreply.github.com>
2023-05-04 09:59:18 +0800

    minor page fixes

--- webpack.config.js
+++ webpack.config.js
@@ -3,84 +3,81 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+    //basics
+    mode: "development",
+    entry: {
+        main: path.resolve(__dirname, "src/js/main.js"),
+        index: path.resolve(__dirname, "src/js/index.js"),
+        portfoliodetails: path.resolve(__dirname, "src/js/portfolio-details.js"),
+        innerpage: path.resolve(__dirname, "src/js/inner-page.js"),
+        signin: path.resolve(__dirname, "src/js/signin.js"),
+        loginadmin: path.resolve(__dirname, "src/js/loginadmin.js"),
+        adminmain: path.resolve(__dirname, "src/js/adminmain.js"),
+        passengerhome: path.resolve(__dirname, "src/js/passenger-home.js"),
+        driverhome: path.resolve(__dirname, "src/js/driver-home.js"),
 
+        //home: path.resolve(__dirname, "src/js/home.js"),
+        //index: path.resolve(__dirname, "src/js/index.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
+        //assetModuleFilename: "img/[name].[ext]",
+        clean: true,
+    },
 
-  //basics
-  mode: "development",
-  entry: {
-    main: path.resolve(__dirname, "src/js/main.js"),
-    index: path.resolve(__dirname, "src/js/index.js"),
-    portfoliodetails: path.resolve(__dirname, "src/js/portfolio-details.js"),
-    innerpage: path.resolve(__dirname, "src/js/inner-page.js"),
-    signin: path.resolve(__dirname, "src/js/signin.js"),
-    loginadmin: path.resolve(__dirname, "src/js/loginadmin.js"),
-    adminmain: path.resolve(__dirname, "src/js/adminmain.js"),
-    passengerhome: path.resolve(__dirname, "src/js/passenger-home.js"),
-    driverhome: path.resolve(__dirname, "src/js/driver-home.js"),
+    devtool: "inline-source-map",
+    devServer: {
+        static: path.join(__dirname, "dist"),
+        //contentBase: path.resolve(__dirname, "dist"),
+        //port: 5001,
+        open: true,
+        hot: true,
+    },
 
+    //loaders
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(svg|ico|png|webp|jpg|jpeg|gif|woff2?)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "img/[name][ext]",
+                },
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 8192,
+                    },
+                },
+            },
+        ],
+    },
+    //plugins
+    plugins: [
+        //do not touch
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }),
 
-    //home: path.resolve(__dirname, "src/js/home.js"),
-    //index: path.resolve(__dirname, "src/js/index.js"),
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
-    //assetModuleFilename: "img/[name].[ext]",
-    clean: true,
-  },
-
-  devtool: "inline-source-map",
-  devServer: {
-    static: path.join(__dirname, "dist"),
-    //contentBase: path.resolve(__dirname, "dist"),
-    //port: 5001,
-    open: true,
-    hot: true,
-  },
-
-  //loaders
-  module: {
-    rules: [
-      {
-        test: /\.(scss|css)$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-      {
-        test: /\.html$/,
-        use: ["html-loader"],
-      },
-      {
-        test: /\.(svg|ico|png|webp|jpg|jpeg|gif|woff2?)$/,
-        type: "asset/resource",
-        generator: {
-          filename: "img/[name][ext]",
-        },
-        parser: {
-          dataUrlCondition: {
-            maxSize: 8192,
-          },
-        },
-      },
-    ],
-  },
-  //plugins
-  plugins: [
-    //do not touch
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-    }),
-
-    /*ADD NEW HTMLWEBPACKPLUGIN WHEN NEW HTML ADDED (COPY PASTE)
+        /*ADD NEW HTMLWEBPACKPLUGIN WHEN NEW HTML ADDED (COPY PASTE)
             new HtmlWebpackPlugin({
             filename: "index.html",
             template: path.resolve(__dirname, "src/html/index.html"),
@@ -88,47 +85,45 @@ module.exports = {
         }),
         */
 
-
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: path.resolve(__dirname, "src/html/index.html"),
-      chunks: ["index"],
-    }),
-    new HtmlWebpackPlugin({
-      filename: "inner-page.html",
-      template: path.resolve(__dirname, "src/html/inner-page.html"),
-      chunks: ["innerpage"],
-    }),
-    new HtmlWebpackPlugin({
-      filename: "portfolio-details.html",
-      template: path.resolve(__dirname, "src/html/portfolio-details.html"),
-      chunks: ["portfoliodetails"],
-    }),
-    new HtmlWebpackPlugin({
-      filename: "signin.html",
-      template: path.resolve(__dirname, "src/html/signin.html"),
-      chunks: ["signin"],
-    }),
-    new HtmlWebpackPlugin({
-      filename: "loginadmin.html",
-      template: path.resolve(__dirname, "src/html/loginadmin.html"),
-      chunks: ["loginadmin"],
-    }),
-    new HtmlWebpackPlugin({
-      filename: "passenger-home.html",
-      template: path.resolve(__dirname, "src/html/passenger-home.html"),
-      chunks: ["passengerhome"],
-    }),
-    new HtmlWebpackPlugin({
-      filename: "driver-home.html",
-      template: path.resolve(__dirname, "src/html/driver-home.html"),
-      chunks: ["driverhome"],
-    }),
-    new HtmlWebpackPlugin({
-        filename: "adminmain.html",
-        template: path.resolve(__dirname, "src/html/adminmain.html"),
-        chunks: ["adminmain"],
-      }),
-  ],
-
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: path.resolve(__dirname, "src/html/index.html"),
+            chunks: ["index"],
+        }),
+        new HtmlWebpackPlugin({
+            filename: "inner-page.html",
+            template: path.resolve(__dirname, "src/html/inner-page.html"),
+            chunks: ["innerpage"],
+        }),
+        new HtmlWebpackPlugin({
+            filename: "portfolio-details.html",
+            template: path.resolve(__dirname, "src/html/portfolio-details.html"),
+            chunks: ["portfoliodetails"],
+        }),
+        new HtmlWebpackPlugin({
+            filename: "signin.html",
+            template: path.resolve(__dirname, "src/html/signin.html"),
+            chunks: ["signin"],
+        }),
+        new HtmlWebpackPlugin({
+            filename: "loginadmin.html",
+            template: path.resolve(__dirname, "src/html/loginadmin.html"),
+            chunks: ["loginadmin"],
+        }),
+        new HtmlWebpackPlugin({
+            filename: "passenger-home.html",
+            template: path.resolve(__dirname, "src/html/passenger-home.html"),
+            chunks: ["passengerhome"],
+        }),
+        new HtmlWebpackPlugin({
+            filename: "driver-home.html",
+            template: path.resolve(__dirname, "src/html/driver-home.html"),
+            chunks: ["driverhome"],
+        }),
+        new HtmlWebpackPlugin({
+            filename: "adminmain.html",
+            template: path.resolve(__dirname, "src/html/adminmain.html"),
+            chunks: ["adminmain"],
+        }),
+    ],
 };
