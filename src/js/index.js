f95cd76523fac9321a835d9ab6bc23d9c5d98735
XNORF <78629634+XNORF@users.noreply.github.com>
2023-05-04 15:45:01 +0800

    signin extra

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,11 @@ main.onAuthStateChanged(main.auth, (user) => {
     if (user) {
         main.getDoc(main.doc(main.db, "users", user.uid)).then((doc) => {
             userData = doc.data();
+            if (userData.type == "passenger") {
+                window.location.href = "/passenger-home.html";
+            } else if (userData.type == "driver") {
+                window.location.href = "/driver-home.html";
+            }
         });
     } else {
     }
--- src/js/signin.js
+++ src/js/signin.js
@@ -5,6 +5,7 @@ main.onAuthStateChanged(main.auth, (user) => {
     if (user) {
         main.getDoc(main.doc(main.db, "users", user.uid)).then((doc) => {
             userData = doc.data();
+            window.location.href = "/index.html";
         });
     } else {
     }
@@ -20,8 +21,8 @@ loginForm.addEventListener("submit", (e) => {
     const password = loginForm.password.value;
     main.signInWithEmailAndPassword(main.auth, email, password)
         .then((cred) => {
-            alert(cred.user.email);
-            console.log(main.auth.currentUser);
+            alert("Sign in successful");
+            window.location.href = "/index.html";
         })
         .catch((error) => {
             alert(error.message);
