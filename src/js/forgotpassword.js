2769578dee7a78ac7c54447695fa2c9a11ed6f88
mikhailshahmie <mikhail.shahmie@gmail.com>
2023-05-16 14:51:56 +0800

    Update forgotpassword.js

--- src/js/forgotpassword.js
+++ src/js/forgotpassword.js
@@ -1,2 +1,31 @@
 ////////////////////////////MUST HAVE////////////////////////////
 const main = require("./main.js");
+
+const resetForm = document.querySelector("#resetForm");
+
+resetForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = resetForm.email.value;
+    let emailRegEx = /[^ ]@graduate.utm.my\i*$/;
+    if (emailRegEx.test(email)) {
+        main.sendPasswordResetEmail(main.auth, email)
+            .then((cred) => {
+                main.onAuthStateChanged(main.auth, (user) => {
+                    if (user) {
+                        if (!user.emailVerified) {
+                            alert("Please check your email for verification (Please check in spam too if you don't see it)");
+                        } else {
+                            alert("Reset Form has been send to you!. Please check your email.");
+                            resetForm.reset();
+                        }
+                    } else {
+                    }
+                });
+            })
+            .catch((error) => {
+                alert(error.message);
+            });
+    } else {
+        alert("Please enter valid UTM student email");
+    }
+});
