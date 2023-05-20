5d468768f6befc0766364a66a852585e1b8c3056
XNORF <78629634+XNORF@users.noreply.github.com>
2023-05-20 11:04:30 +0800

    driver application test setup

--- src/js/driver-application.js
+++ src/js/driver-application.js
@@ -2,16 +2,23 @@
 const main = require("./main.js");
 let userData;
 main.onAuthStateChanged(main.auth, (user) => {
-  if (user) {
-    main.getDoc(main.doc(main.db, "users", user.uid)).then((doc) => {
-      userData = doc.data();
-      /*if (userData.type == "passenger") {
-        window.location.href = "/passenger-home.html";
-      } else if (userData.type == "driver") {
-        window.location.href = "/driver-home.html";
-      }*/
-    });
-  } else {
-  }
+    if (user) {
+        main.getDoc(main.doc(main.db, "users", user.uid)).then((doc) => {
+            userData = doc.data();
+            ////////////////
+        });
+    } else {
+        window.location.href = "/signin.html";
+    }
 });
 /////////////////////////////////////////////////////////////////
+
+const requestForm = document.querySelector("#requestForm");
+
+requestForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const test = requestForm.licenseImg.files;
+    console.log(test[0].name);
+    //${URL.createObjectURL(test[0])};
+    console.log();
+});
