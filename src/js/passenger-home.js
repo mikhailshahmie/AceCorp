e8c53b9a6be4f698eec2500bf1eaa91a7d4d8850
XNORF <78629634+XNORF@users.noreply.github.com>
2023-05-26 01:13:51 +0800

    fix admin login passenger pages bug

--- src/js/passenger-home.js
+++ src/js/passenger-home.js
@@ -3,6 +3,10 @@ const main = require("./main.js");
 let userData;
 main.onAuthStateChanged(main.auth, (user) => {
     if (user) {
+        let emailRegEx = /[^ ]@graduate.utm.my\i*$/;
+        if (!emailRegEx.test(user.email)) {
+            window.location.href = "/adminmain.html";
+        }
         main.getDoc(main.doc(main.db, "users", user.uid)).then((doc) => {
             userData = doc.data();
             //CHANGE DRIVER OR PASSENGER BUTTON
