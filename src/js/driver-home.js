94b37990c632f4bb2bcd843c364c2942aa47a551
XNORF <78629634+XNORF@users.noreply.github.com>
2023-06-19 23:06:23 +0800

    driver home view booking list

--- src/js/driver-home.js
+++ src/js/driver-home.js
@@ -1,4 +1,5 @@
 ////////////////////////////MUST HAVE////////////////////////////
+import $ from "jquery";
 const main = require("./main.js");
 let userData;
 main.onAuthStateChanged(main.auth, (user) => {
@@ -24,3 +25,48 @@ signoutbtn.addEventListener("click", (e) => {
             console.log(error.message);
         });
 });
+
+$(function () {
+    let bookingList = [];
+    let index = 0;
+    const bookingListForm = document.querySelector("#bookingListForm");
+
+    const q = main.query(main.bookingDB, main.where("status", "==", "waiting"));
+    main.onSnapshot(q, (snapshot) => {
+        bookingList = [];
+        if (!snapshot.empty) {
+            snapshot.docs.forEach((doc) => {
+                bookingList.push({ ...doc.data(), bookingId: doc.id });
+            });
+        }
+        updateCurrentList(bookingList[0]);
+
+        $("#nextBtn").click((e) => {
+            e.preventDefault();
+            if (index < bookingList.length - 1) {
+                index++;
+            } else {
+                index = 0;
+            }
+            updateCurrentList(bookingList[index]);
+        });
+
+        $("#previousBtn").click((e) => {
+            e.preventDefault();
+            if (index > 0) {
+                index--;
+            } else {
+                index = bookingList.length - 1;
+            }
+            updateCurrentList(bookingList[index]);
+        });
+    });
+});
+
+function updateCurrentList(currentView) {
+    bookingListForm.origin.value = currentView.from;
+    bookingListForm.destination.value = currentView.destination;
+    bookingListForm.person.value = currentView.person;
+    bookingListForm.datetime.value = currentView.datetime;
+    bookingListForm.notes.value = currentView.notes;
+}
